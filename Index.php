046e84a764282fbbee568d21deb43b396e8a1e82
mitul <mrp68@njit.edu>
2019-06-21 16:06:37 -0400

    Created CSV file and first code for to read file then display on localhost

--- Index.php
+++ Index.php
@@ -5,3 +5,30 @@
  * Date: 6/21/19
  * Time: 2:40 PM
  */
+main::First("info.csv");
+class main
+{
+/*This will collect my functions and display */
+    static public function first($filename)
+    {
+      $records = csv::getfollowing($filename);
+      print_r($records);
+    }
+}
+class csv
+{
+    static public function getfollowing($filename)
+    {
+        $file = fopen($filename,"r");// this will help me to declare a csv file to reconize in following code
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            $records[] = $record;
+        }
+
+        fclose($file);
+        return($records);//return to main function
+    }
+}
+
